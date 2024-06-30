1892845dbbf827b71342b24223b82ba8af2ef8fc
MonikaS5 <monika.bhagade@gmail.com>
2024-06-30 21:05:07 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// server/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const uri = process.env.DB_CONNECT;
+
+mongoose.connect(uri, { useNewUrlParser: true, 
+                                    useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err));
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
